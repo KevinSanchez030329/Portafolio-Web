4c58923e083d8070d0097568ededef01750dcb48
Kevin J. Sanchez <kevinsanchez030329@gmail.com>
2022-03-02 10:19:10 -0600

    version alpha 0.1004

--- email.php
+++ email.php
@@ -1,10 +1,9 @@
 <?php
-    $destino= "srkevin7000@gmail.com";
     $name= $_POST["name"];
     $asunto= $_POST["asunto"];
     $email= $_POST["email"];
     $mensaje= $_POST["mensaje"];
     $content= "name:" . $name . "\nemail:" . $email . "\nmensaje:" . $mensaje;
-    mail($destino,$asunto,$content);
+    mail("kevinsanchez030329@gmail.com",$asunto,$content);
     header ("Location:gracias.html");
 ?>
