d463e4da0dd00247673a27ec80ed9bf03e0e974f
Kevin J. Sanchez <kevinsanchez030329@gmail.com>
2022-05-17 10:29:58 -0500

    Version Pre-beta 1.2

--- email.php
+++ email.php
@@ -1,9 +1,10 @@
 <?php
-    $name= $_POST["name"];
-    $asunto= $_POST["asunto"];
-    $email= $_POST["email"];
-    $mensaje= $_POST["mensaje"];
-    $content= "name:" . $name . "\nemail:" . $email . "\nmensaje:" . $mensaje;
-    mail("kevinsanchez030329@gmail.com",$asunto,$content);
-    header ("Location:gracias.html");
+    $email = "srkevin7000@gmail.com";
+    $name = $_POST["name"];
+    $proyect = $_POST["proyect"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+    $content = "Nombre: " . $name . "\nProyecto: " . $proyect . "\nCorreo/Numero:" . $email . "\nMensaje: " . $mensaje;
+    mail($email, $proyect, $content);
+    header ("Location:form.html");
 ?>
